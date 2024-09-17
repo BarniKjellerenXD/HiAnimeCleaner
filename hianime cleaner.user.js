b216f54d8edb565d45b5b1e0e03a6fbd8260ead8
BarniKjellerenXD <barnikjelleren@proton.me>
2024-09-17 20:36:21 +0200

    HiAnimeCleaner.user.js
    the main user script file for the HiAnime Cleaner

---
+++ hianime cleaner.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         hianime cleaner
+// @namespace    http://tampermonkey.net/
+// @version      2024-08-27
+// @description  try to take over the world!
+// @author       You
+// @match        https://hianime.to/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=hianime.to
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.getElementById("logo").setAttribute("href", "/home")
+    document.getElementById("discussion").remove()
+    document.querySelector(".share-buttons.share-buttons-home").remove()
+    document.getElementById("anime-trending").remove()
+    document.getElementById("anime-featured").remove()
+    document.querySelector(".block_area-genres").remove()
+    document.querySelector(".share-buttons.share-buttons-min").remove()
+})();
